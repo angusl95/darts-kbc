db045604d9ec1227dcdaa13b7951ff990eeb070e
Angus Lamb <angusjameslamb@gmail.com>
2019-06-25 14:30:02 +0100

    add typing

--- kbc/combined/train_existing_models.py
+++ kbc/combined/train_existing_models.py
@@ -14,6 +14,7 @@ import torch.utils
 #import torchvision.datasets as dset
 #import torchvision.transforms as transforms
 import torch.backends.cudnn as cudnn
+from typing import Dict
 from datasets import Dataset
 from models import CP, ComplEx
 from regularizers import N2, N3
