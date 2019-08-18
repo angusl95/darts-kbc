0f0a970d67ddb3d431969edfd3a7b8447b38e996
Angus Lamb <angusjameslamb@gmail.com>
2019-08-18 21:43:55 +0100

    fix typo

--- kbc/combined_linear/operations.py
+++ kbc/combined_linear/operations.py
@@ -67,7 +67,7 @@ class SepConv(nn.Module):
       nn.Conv2d(C_in, C_in, kernel_size=kernel_size, stride=1, padding=padding, groups=C_in, bias=False),
       nn.Conv2d(C_in, C_out, kernel_size=1, padding=0, bias=False),
       nn.BatchNorm2d(C_out, affine=affine),
-      nn.ReLU(inplace=False)
+      nn.ReLU(inplace=False),
       nn.Dropout2d(p=0.2),
       )
 
