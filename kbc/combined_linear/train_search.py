f961c9388a566542840d95392ca5cef5d4d6d62d
Angus Lamb <angusjameslamb@gmail.com>
2019-08-24 14:23:02 +0100

    save embeddings on train search

--- kbc/combined_linear/train_search.py
+++ kbc/combined_linear/train_search.py
@@ -190,6 +190,7 @@ def main():
         patience +=1
 
     utils.save(model, os.path.join(args.save, 'weights.pt'))
+    torch.save(model.embeddings, os.path.join(args.save, 'embeddings.pt'))
     
 def train_epoch(train_examples,train_queue, valid_queue,
   model, architect, criterion, optimizer: optim.Optimizer, 
