81aa6b1d94d6796aa89e7b88d79b6b50e4c89eb3
Angus Lamb <angusjameslamb@gmail.com>
2019-08-28 22:01:46 +0100

    add random baselines

--- kbc/combined_linear/genotypes.py
+++ kbc/combined_linear/genotypes.py
@@ -47,3 +47,8 @@ FBNet_V1 = Genotype(normal=[('conv_3x3', 0), ('identity', 1), ('conv_5x5', 2), (
 
 WNNet_V2 = Genotype(normal=[('conv_1x5', 0), ('identity', 1), ('dil_conv_5x5', 2), ('identity', 3), ('dil_conv_5x5', 4)], normal_concat=[5])
 FBNet_V2 = Genotype(normal=[('identity', 0), ('identity', 1), ('dil_conv_5x5', 2), ('identity', 3), ('identity', 4)], normal_concat=[5])
+
+rand1 = Genotype(normal=[('conv_3x3', 0), ('conv_5x5', 1), ('dil_conv_5x5', 2), ('conv_1x5', 3), ('conv_3x3', 4)], normal_concat=[5])
+rand2 = Genotype(normal=[('identity', 0), ('conv_5x5', 1), ('conv_3x3', 2), ('conv_5x1', 3), ('conv_5x5', 4)], normal_concat=[5])
+rand3 = Genotype(normal=[('conv_5x5', 0), ('conv_3x3', 1), ('conv_1x5', 2), ('dil_conv_5x5', 3), ('conv_3x3', 4)], normal_concat=[5])
+rand4 = Genotype(normal=[('identity', 0), ('dil_conv_5x5', 1), ('conv_1x5', 2), ('conv_1x5', 3), ('conv_3x3', 4)], normal_concat=[5])
