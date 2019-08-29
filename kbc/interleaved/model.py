371722bf998693c06f6e5d52f050e6718c953255
Angus Lamb <angusjameslamb@gmail.com>
2019-08-29 12:52:17 +0100

    fix channels

--- kbc/interleaved/model.py
+++ kbc/interleaved/model.py
@@ -149,7 +149,7 @@ class NetworkKBC(KBCModel):
     self.input_drop = torch.nn.Dropout(p=0.2)
     self.input_bn = torch.nn.BatchNorm2d(1)
 
-    self.projection = nn.Linear(self.emb_dim*2*self._C//4, self.emb_dim)#, bias=False)
+    self.projection = nn.Linear(self.emb_dim*2*self._C, self.emb_dim)#, bias=False)
 
     self.output_bn = nn.BatchNorm1d(self.emb_dim)
     self.output_drop = torch.nn.Dropout(p=0.3)
