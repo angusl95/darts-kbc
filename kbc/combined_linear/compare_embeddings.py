9b790ff090c70c1d390f30c8cc767cba4e3fd43d
Angus Lamb <angusjameslamb@gmail.com>
2019-09-02 01:21:59 +0100

    update embedding training script

--- kbc/combined_linear/compare_embeddings.py
+++ kbc/combined_linear/compare_embeddings.py
@@ -1,6 +1,8 @@
 import os
 
-for op in ['identity', 'conv', 'dil_conv', 'max_pool', 'avg_pool']:
+for emb_op in ['identity', 'conv', 'dil_conv', 'max_pool', 'avg_pool']:
+	for arch_op in ['identity', 'conv', 'dil_conv', 'max_pool', 'avg_pool']:
+			print('embeddings:', emb_op, 'architecture:', arch_op)
 			contents = """#$ -l gpu=1
 #$ -l tmem=8G
 #$ -l h_rt=2:00:00
@@ -13,7 +15,7 @@ hostname
 python /home/angulamb/darts-kbc/kbc/interleaved/train_from_embeddings.py --embeddings {} --dataset WN18RR --learning_rate 1e-4 \
 --learning_rate_min 1e-4 --emb_dim 200 --channels 32 --arch {} --epochs 200 --batch_size 256 \
 --reg 0 --report_freq 3 --optimizer Adam --layers 1 --weight_decay 1e-3 --seed 123 \
-			""".format(op, 'embeddings_' + op + '.pt')
+			""".format('embeddings_' + emb_op + '.pt', arch_op)
 			with open("autosub.sub", "w") as file:
 				file.write(contents)
 			os.system("qsub autosub.sub")
